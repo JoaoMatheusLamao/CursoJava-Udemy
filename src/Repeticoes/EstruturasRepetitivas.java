2be8e7e5a9b19d9532a5149cc028b8c73ec71fe8
João Matheus Lamão <joaomatheuslamao9@gmail.com>
2023-06-12 21:06:25 -0300

    Aula 54 - doWilhe

--- src/Repeticoes/EstruturasRepetitivas.java
+++ src/Repeticoes/EstruturasRepetitivas.java
@@ -4,20 +4,43 @@ import java.util.Scanner;
 
 public class EstruturasRepetitivas {
     public static void main(String[] args) {
-        enquanto();
+        conversorMedida();
 
     }
 
 
     public static void enquanto(){
         Scanner sc = new Scanner(System.in);
-        int a = 1;
+        int a = sc.nextInt();
         int b = 0;
 
         while (a != 0){
-            a = sc.nextInt();
             b += a;
+            a = sc.nextInt();
         }
         System.out.println(b);
+    }
+
+    public static void para(){
+        for (int x = 0; x <= 10; x+=2){
+            System.out.println(x);
+        }
+    }
+
+    public static void conversorMedida(){
+        Scanner sc = new Scanner(System.in);
+        char op;
+        do {
+            System.out.print("Entre com a temperatura em Celsius: ");
+            double temp = sc.nextDouble();
+
+            double f = ((9*temp)/5)+32;
+            System.out.printf("%.1f C é igual a %.2f F\n\n", temp, f);
+
+            System.out.print("Deseja calcular uma nova temperatura? (S/N): ");
+            op = sc.next().charAt(0);
+
+        } while (op == 's' || op == 'S');
+        sc.close();
     }
 }
